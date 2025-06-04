67e75bd359280b2c561101b790098ffcf11a6f87
safayet nur <safayetnurelectra@gmail.com>
2025-06-04 15:04:06 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ const recipes = [
     imgSrc: "../images/home-page-images/Creamy-Garlic-Chicken-Pan.jpg",
     likes: 120,
     rating: 4.9,
+    selling: 10,
     description: "Indulge in the rich and savory flavors...",
     chefImg: "../images/home-page-images/man1.jpeg",
     chefName: "Anthony",
@@ -28,6 +29,7 @@ const recipes = [
     imgSrc: "../images/home-page-images/fruit-salad.jpg",
     likes: 95,
     rating: 4.7,
+    selling: 13,
     description: "Enjoy bold flavors with these spicy noodles...",
     chefImg: "../images/home-page-images/woman2.jpeg",
     chefName: "Sophia",
@@ -38,6 +40,7 @@ const recipes = [
     imgSrc: "../images/home-page-images/salmon.jpeg",
     likes: 134,
     rating: 4.8,
+    selling: 14,
     description: "A simple yet delicious grilled salmon recipe...",
     chefImg: "../images/home-page-images/man1.jpeg",
     chefName: "Chef Oliver",
@@ -48,41 +51,42 @@ const recipes = [
     imgSrc: "../images/home-page-images/roll.jpeg",
     likes: 95,
     rating: 4.7,
+    selling: 16,
     description: "Enjoy bold flavors with these spicy noodles...",
     chefImg: "../images/home-page-images/woman1.jpeg",
     chefName: "Sophia",
     time: "25mins.",
   },
-  {
-    title: "Egg Power Bowl",
-    imgSrc: "../images/home-page-images/Egg-bowl.jpeg",
-    likes: 78,
-    rating: 4.5,
-    description:
-      "A healthy and filling bowl of eggs, avocado, and greens to...",
-    chefImg: "../images/home-page-images/man2.jpeg",
-    chefName: "Chef Liam",
-    time: "20mins.",
-  },
 
   {
     title: "Masala Chicken Curry",
-    imgSrc: "../images/home-page-images/masala-chiken.jpeg",
+    imgSrc: "../images/home-page-images/masala-chicken.jpg",
     likes: 212,
     rating: 4.9,
-    description:
-      "Rich, spicy and creamy chicken masala made with traditional Indian spices.",
+    selling: 20,
+    description: "Rich, spicy and creamy chicken masala made...",
     chefImg: "../images/home-page-images/woman2.jpeg",
     chefName: "Chef Priya",
     time: "45mins.",
   },
   {
+    title: "Egg Power Bowl",
+    imgSrc: "../images/home-page-images/rice-bwl.jpg",
+    likes: 78,
+    rating: 4.5,
+    selling: 21,
+    description: "A healthy and filling bowl of eggs, avocado...",
+    chefImg: "../images/home-page-images/man2.jpeg",
+    chefName: "Chef Liam",
+    time: "20mins.",
+  },
+  {
     title: "Fresh Fruit Salad",
-    imgSrc: "../images/home-page-images/fruit-salad.jpeg",
+    imgSrc: "../images/home-page-images/fruit-salad2.jpg",
     likes: 150,
     rating: 4.6,
-    description:
-      "A colorful mix of fresh seasonal fruits topped with a honey-lime glaze.",
+    selling: 32,
+    description: "A colorful mix of fresh seasonal fruits topped...",
     chefImg: "../images/home-page-images/woman1.jpeg",
     chefName: "Chef Emma",
     time: "15mins.",
@@ -91,6 +95,8 @@ const recipes = [
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("recipeContainer");
+  const bestSellingContainer = document.getElementById("bestSellingContainer");
+
   recipes.slice(0, 3).forEach((recipe) => {
     const card = document.createElement("div");
     card.className = "recipe-card";
@@ -123,4 +129,40 @@ document.addEventListener("DOMContentLoaded", () => {
         `;
     container.appendChild(card);
   });
+
+  recipes
+    .sort((a, b) => b.selling - a.selling)
+    .slice(0, 3)
+    .forEach((recipe) => {
+      const card = document.createElement("div");
+      card.className = "recipe-card";
+      card.innerHTML = `
+          <div class="book-mark">
+            <i class="fa-solid fa-bookmark"></i>
+          </div>
+          <div class="recipe-img">
+            <img src="${recipe.imgSrc}" alt="${recipe.title}" />
+          </div>
+          <div class="recipe-details">
+            <div class="recipe-reaction">
+              <span><i class="fa-solid fa-thumbs-up"></i> ${recipe.likes} likes</span>
+              <span><i class="fa-solid fa-star"></i>${recipe.rating}</span>
+            </div>
+            <p class="recipe-title">${recipe.title}</p>
+            <p class="recipe-p">${recipe.description}</p>
+            <div class="card-bottom">
+              <div class="chef-img-name">
+                <div class="chef-img">
+                  <img src="${recipe.chefImg}" alt="${recipe.chefName}" />
+                </div>
+                <p>${recipe.chefName}</p>
+              </div>
+              <p class="card-bottom-time">
+                <i class="fa-solid fa-clock"></i> ${recipe.time}
+              </p>
+            </div>
+          </div>
+        `;
+      bestSellingContainer.appendChild(card);
+    });
 });
